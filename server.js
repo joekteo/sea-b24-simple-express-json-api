a60d019b0023f13d6d781eb837eb415156b0aab0
Joe Teo <revelate@live.com>
2014-11-01 22:33:30 -0700

    create api with testing

--- server.js
+++ server.js
@@ -1,2 +1,30 @@
+// BASE SETUP
 var express = require('express');
-//your code here
+var moment  = require('moment');
+var app     = express();
+var port    = process.env.PORT || 3001;
+
+// ROUTES
+// get an instance of router
+var router = express.Router();
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
+// route to display local time
+router.get('/time', function (req, res) {
+  res.json('Local time is ' + moment().format("YYYY-MM-DD HH:mm"));
+});
+
+// route with parameters (http://localhost:3001/hello/:name)
+router.get('/greet/:name', function(req, res) {
+  res.json('Greetings ' + req.params.name + '!');
+});
+
+app.use('/', router);
+
+// START SERVER
+app.listen(port);
--- test/api_test.js
+++ test/api_test.js
@@ -1,8 +1,31 @@
-var chai = require('chai');
+var chai     = require('chai');
 var chaihttp = require('chai-http');
 chai.use(chaihttp);
+var expect   = require('chai').expect;
+var assert   = chai.assert;
+var app      = 'http://localhost:' + (process.env.PORT || 3001);
+var moment   = require('moment');
 
 describe('Simple JSON API', function() {
-  it('should send the local time');
-  it('should greet someone');
+  it('should send the local time', function () {
+    var time = [];
+    chai.request(app)
+      .get('/time')
+      .end( function (err, res) {
+        expect(res).to.have.status(200);
+        expect(req.params.name).to.be.a('object');
+        expect('/time').to.equal(moment().format("YYYY-MM-DD HH:mm"));
+      });
+  });
+
+  it('should greet someone', function () {
+    chai.request(app)
+      .get('/greet/:name')
+      .end( function (err, res) {
+        expect(res).to.have.status(200);
+        expect(req.params.name).to.not.be.instanceof('number');
+        expect(res.body).to.have.property('greet');
+        expect(res.body.msg).to.eql('Greetings ' + req.params.name + '!');
+      });
+  });
 });
